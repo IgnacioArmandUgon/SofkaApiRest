be6629ca027093ec9362ce7d79d73846f1115631
Ignacio Armand-ugón <Matias4371@gmail.com>
2022-02-18 02:15:54 -0300

    Documentación añadida en UsuarioService

--- src/main/java/com/ApiRest/services/UsuarioService.java
+++ src/main/java/com/ApiRest/services/UsuarioService.java
@@ -13,22 +13,46 @@ public class UsuarioService {
     @Autowired
     UsuarioRepository usuarioRepository;
 
+    /**
+     * Retorna un ArrayList con todos los usuarios
+     * @return
+     */
     public ArrayList<UsuarioModel> obtenerUsuarios(){
         return (ArrayList<UsuarioModel>) usuarioRepository.findAll();
     }
 
+    /**
+     * Guarda un usuario dado
+     * @param usuarioModel
+     * @return
+     */
     public UsuarioModel guardarUsuario(UsuarioModel usuarioModel) {
         return usuarioRepository.save(usuarioModel);
     }
 
-
-
+    /**
+     * Retorna un Optional con lo usuarios que coincidan con la id indicada
+     * @param id
+     * @return
+     */
     public Optional<UsuarioModel> obtenerPorId(Long id){
         return usuarioRepository.findById(id);
     }
+
+    /**
+     * Retorna un ArrayList con los usuario que tengan asignada la prioridad indicada
+     * @param prioridad
+     * @return
+     */
     public ArrayList<UsuarioModel> obtenerPorPrioridad(Integer prioridad){
         return usuarioRepository.findByPrioridad(prioridad);
     }
+
+    /**
+     * Elimina al usuario con la id indicada. Retorna un booleano confirmando que el metodo funcionó
+     * @param id
+     * @return
+     */
     public boolean eliminarUsuario(Long id){
         try{
             usuarioRepository.deleteById(id);
@@ -37,6 +61,11 @@ public class UsuarioService {
             return false;
         }
     }
+
+    /**
+     * Elimina a todos los usuarios
+     * @return
+     */
     public boolean eliminarUsuarios(){
         try{
             usuarioRepository.deleteAll();
